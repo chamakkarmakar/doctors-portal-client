d33cc3d1454988ba517b1fd1d08548394635a6b9
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-05-13 18:08:18 +0600

    google signin authentication

--- src/Pages/LoginRegister/Login/Login.js
+++ src/Pages/LoginRegister/Login/Login.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    
+    // if (error) {
+    //     return (
+    //       <div>
+    //         <p>Error: {error.message}</p>
+    //       </div>
+    //     );
+    // }
+    if (user) {
+        return (
+          <div>
+            <p>Signed In User: {user.email}</p>
+          </div>
+        );
+      }
+    
+    const handleLogin = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email, password);
+
+    }
     return (
         <div className='container w-3/4 mx-auto mt-10 md:w-1/2 flex shadow-2xl '>
-            <form className='mx-7 py-7 w-1/2'>
+            <form className='mx-7 py-7 w-1/2' onSubmit={handleLogin}>
                 <h2 className='text-center text-2xl uppercase font-extrabold mb-5 text-secondary'>Sign In</h2>
-                <input type="email" name='email' className='py-2 mb-5 px-2 rounded-md w-full outline-none border border-gray-300' placeholder='@email.com' />
-                <input type="password" name='password' className='py-2 mb-5 px-2 rounded-md w-full outline-none border border-gray-300' placeholder='**********' />
+                <input type="email" name='email' className='py-2 mb-5 px-2 rounded-md w-full outline-none border border-gray-300' ref={emailRef} placeholder='@email.com' />
+                <input type="password" name='password' className='py-2 mb-5 px-2 rounded-md w-full outline-none border border-gray-300' ref={passwordRef} placeholder='**********' />
                 <div className='text-center mb-5'>
-                    <input type="submit" className='bg-gradient-to-r from-secondary to-primary px-5 py-2 rounded-3xl text-white font-bold' value="SIGN IN" />
+                    <input type="submit" className='bg-gradient-to-r from-secondary to-primary px-5 cursor-pointer py-2 rounded-3xl text-white font-bold' value="SIGN IN" />
                 </div>
                 <p className='text-sm'>New to Doctors Portal? <Link to='/register' className='text-secondary font-bold'>Create New Account</Link> </p>
             </form>
--- src/Pages/LoginRegister/SocialLogin/SocialLogin.js
+++ src/Pages/LoginRegister/SocialLogin/SocialLogin.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { FaFacebook } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import auth from '../../../firebase.init';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user1, loading1, error1] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, user2, loading2, error2] = useSignInWithFacebook(auth);
     return (
         <div style={{ backgroundImage: `url("https://img.freepik.com/free-photo/watercolor-blue-background-texture_145343-1136.jpg")` }} className='h-full'>
             <div className='py-20'>
                 <div className='flex flex-col items-center justify-items-center '>
-                    <h5 className='bg-white mb-5 px-7 py-2'><FcGoogle className='inline w-6 h-6 mr-3'></FcGoogle> Sign in with Google</h5>
-                    <h5 className='bg-white mb-5 px-5 py-2'><FaFacebook className='inline w-6 h-6 mr-3 text-blue-500'></FaFacebook> Sign in with Facebook</h5>
+                    <button className='bg-white mb-5 px-7 py-2' onClick={() => signInWithGoogle()}><FcGoogle className='inline w-6 h-6 mr-3'></FcGoogle> Sign in with Google</button>
+                    <button className='bg-white mb-5 px-5 py-2' onClick={() => signInWithFacebook()}><FaFacebook className='inline w-6 h-6 mr-3 text-blue-500'></FaFacebook> Sign in with Facebook</button>
                 </div>
             </div>
 
